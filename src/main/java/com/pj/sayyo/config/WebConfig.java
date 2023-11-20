f7fff763e5579a8d38f292d1e025216375089ada
youngsil <koo1175@naver.com>
2023-11-21 01:52:34 +0900

    config수정(2)

--- src/main/java/com/pj/sayyo/config/WebConfig.java
+++ src/main/java/com/pj/sayyo/config/WebConfig.java
@@ -6,14 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
 }
