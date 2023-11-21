a2b13cdcd5a9e5d89e68b45f69442b1baeba37b1
youngsil <koo1175@naver.com>
2023-11-21 11:05:51 +0900

    chat 다시 작업 후 커밋 예정

--- src/main/java/com/pj/sayyo/model/chat/mapper/ChatMapper.java
+++
@@ -1,9 +0,0 @@
-package com.pj.sayyo.model.chat.mapper;
-
-import com.pj.sayyo.model.chat.dto.ChatDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ChatMapper {
-    ChatDto send(ChatDto chatDto);
-}
