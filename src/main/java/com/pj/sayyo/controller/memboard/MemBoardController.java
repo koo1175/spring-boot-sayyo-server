73c7116d9bd6ca12c3cfd3c290b6479c223c07f1
youngsil <koo1175@naver.com>
2023-11-23 10:38:01 +0900

    chat 수정중 & MEMBOARD 수정(5)

--- src/main/java/com/pj/sayyo/controller/memboard/MemBoardController.java
+++ src/main/java/com/pj/sayyo/controller/memboard/MemBoardController.java
@@ -1,7 +1,8 @@
 package com.pj.sayyo.controller.memboard;
 
-import com.pj.sayyo.model.board.dto.BoardDto;
+import com.pj.sayyo.model.memBoard.dto.MemBoardDto;
 import com.pj.sayyo.service.board.BoardService;
+import com.pj.sayyo.service.memBoard.MemBoardService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +18,14 @@ public class MemBoardController {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    private BoardService boardService;
+    private MemBoardService memBoardService;
 
     @PostMapping("/regist")
     @ResponseBody
-    private HashMap<String, Object> regist(@RequestBody BoardDto boardDto){
+    private HashMap<String, Object> regist(@RequestBody MemBoardDto memboardDto){
         HashMap<String, Object> mv = new HashMap<>();
 
-        int resultCnt = boardService.regist(boardDto);
+        int resultCnt = memBoardService.regist(memboardDto);
         mv.put("result", resultCnt);
         System.out.println(resultCnt);
 
@@ -35,7 +36,7 @@ public class MemBoardController {
     @ResponseBody
     private HashMap<String, Object> findAll(){
         HashMap<String, Object> mv = new HashMap<>();
-        List<BoardDto> list = boardService.findAll();
+        List<MemBoardDto> list = memBoardService.findAll();
 
         mv.put("list", list);
         return mv;
@@ -43,24 +44,24 @@ public class MemBoardController {
 
     @PostMapping("/modify")
     @ResponseBody
-    public void modify(@RequestBody BoardDto boardDto) {
-        int resultCnt2 = boardService.modify(boardDto);
+    public void modify(@RequestBody MemBoardDto memboardDto) {
+        int resultCnt2 = memBoardService.modify(memboardDto);
         System.out.println("수정 완료 여부 : " + resultCnt2);
     }
 
     @PostMapping("/delete")
     @ResponseBody
-    public void delete(@RequestBody BoardDto boardDto) {
-        int resultCnt3 = boardService.delete(boardDto);
+    public void delete(@RequestBody MemBoardDto memboardDto) {
+        int resultCnt3 = memBoardService.delete(memboardDto);
         System.out.println("삭제 완료 여부 : " + resultCnt3);
 
     }
 
     @GetMapping("/findSearch")
     @ResponseBody
-    public void selectFind(@RequestBody BoardDto boardDto) {
+    public void selectFind(@RequestBody MemBoardDto memboardDto) {
         HashMap<String, Object> mv = new HashMap<>();
-        List<BoardDto> list = boardService.findSearch(boardDto);
+        List<MemBoardDto> list = memBoardService.findSearch(memboardDto);
 
         mv.put("list", list);
     }
